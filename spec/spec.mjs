2741a5f75ee1b449ae00364d640894f90e294e8e
Edu <79867404+Uardet99@users.noreply.github.com>
2023-12-12 13:31:02 +0100

    Testing finalizado

--- spec/spec.mjs
+++ spec/spec.mjs
@@ -1,19 +1,24 @@
-import * as Game from "../src/assets/js/game/gameHelpers.js";
+import { Jugador } from "../src/assets/js/game/jugador.js";
+import * as GameHelpers from "../src/assets/js/game/gameHelpers.js";
 import * as Views from "../src/assets/js/views.js";
+import * as GameLogic from "../src/assets/js/game/gameLogic.js";
+import * as Login from "../src/assets/js/login/loginHelpers.js";
+import * as Register from "../src/assets/js/register/registerHelpers.js";
+import * as Profile from "../src/assets/js/profile/profileHelpers.js";
 
 describe("TESTING - Funciones Proyecto", function () {
   describe("plantillaTablero()", function () {
     it("Deberia estar definida", function () {
-      expect(Game.plantillaTablero()).toBeDefined();
+      expect(GameHelpers.plantillaTablero()).toBeDefined();
     });
     it("No deberia estar a null", function () {
-      expect(Game.plantillaTablero()).not.toBeNull();
+      expect(GameHelpers.plantillaTablero()).not.toBeNull();
     });
     it("Deberia devolver un String", function () {
-      expect(Game.plantillaTablero()).toEqual(jasmine.any(String));
+      expect(GameHelpers.plantillaTablero()).toEqual(jasmine.any(String));
     });
     it("Deberia generar un cadena HTML valida", function () {
-      const tableroHTML = Game.plantillaTablero();
+      const tableroHTML = GameHelpers.plantillaTablero();
       const elemento = document.createElement("div");
       elemento.innerHTML = tableroHTML;
       expect(elemento.firstChild).toBeDefined();
@@ -21,27 +26,27 @@ describe("TESTING - Funciones Proyecto", function () {
   });
   describe("imagenAleatorio()", function () {
     it("Deberia retornar un dato de tipo Array", function () {
-      expect(Game.imgAleatoria()).toEqual(jasmine.any(Array));
+      expect(GameHelpers.imgAleatoria()).toEqual(jasmine.any(Array));
     });
     it("Deberia retornar un Array de 20 posiciones", function () {
-      expect(Game.imgAleatoria().length).toBe(20);
+      expect(GameHelpers.imgAleatoria().length).toBe(20);
     });
     it("Deberia estar definido", function () {
-      expect(Game.imgAleatoria()).toBeDefined();
+      expect(GameHelpers.imgAleatoria()).toBeDefined();
     });
     it("No deberia estar a null", function () {
-      expect(Game.imgAleatoria()).not.toBeNull();
+      expect(GameHelpers.imgAleatoria()).not.toBeNull();
     });
   });
   describe("addImagenesPlantilla()", function () {
     it("Deberia agregar imagenes a todos los elementos", function () {
-      Game.addImagenesPlantilla();
+      GameHelpers.addImagenesPlantilla();
       const imagenEnTabla = document.querySelectorAll("td img").length;
       const cantidadElementosTd = document.querySelectorAll("td").length;
       expect(imagenEnTabla).toBe(cantidadElementosTd);
     });
     it("Deberia agregar imagenes con IDs unicos", function () {
-      Game.addImagenesPlantilla();
+      GameHelpers.addImagenesPlantilla();
       const idsImagenes = Array.from(document.querySelectorAll("td img"));
       const sonUnicos = new Set(idsImagenes).size === idsImagenes.length;
       expect(sonUnicos).toBe(true);
@@ -61,5 +66,28 @@ describe("TESTING - Funciones Proyecto", function () {
       expect(Views.reiniciarClick(objeto)).toBeNull();
     });
   });
-  
+
+  describe("loginForm()", function () {
+    it("Deberia devolver un Elemento (div)", function () {
+      expect(Login.loginForm()).toEqual(jasmine.any(Element));
+    });
+    it("Deberia estar definida", function () {
+      expect(Login.loginForm()).toBeDefined();
+    });
+    it("No deberia estar a null", function () {
+      expect(Login.loginForm()).not.toBeNull();
+    });
+  });
+
+  describe("registerForm()", function () {
+    it("Deberia devolver un Elemento (div)", function () {
+      expect(Register.registerForm()).toEqual(jasmine.any(Element));
+    });
+    it("Deberia estar definida", function () {
+      expect(Register.registerForm()).toBeDefined();
+    });
+    it("No deberia estar a null", function () {
+      expect(Register.registerForm()).not.toBeNull();
+    });
+  });
 });
